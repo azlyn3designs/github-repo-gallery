cf0616915faad8b2bfa6d1712f5c5197fc862f93
thea_smith <smith.thea@outlook.com>
2021-09-14 14:49:30 -0600

    Created an async function to collect a list of my github repos and their data, then added a second function to display each repo in unordered list element to only include the repos name

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@
 const overview = document.querySelector(".overview");
 //my GitHub username
 const username = "azlyn3designs";
+//displays a list of my public repos in the unordered list eleement
+const repoList = document.querySelector(".repo-list")
 
 //async function used to collect my profile data from GitHub acct
 const getProfileInfo = async function() {
     const profileInfo = await fetch(`https://api.github.com/users/${username}`);
     const profileData = await profileInfo.json();
-    console.log(profileData);
+    //console.log(profileData);
 
-    displayGHInfo(profileData);
+    displayGHInfo(profileData); //calls function
 };
 
-getProfileInfo();
+getProfileInfo(); //calls function
 
 
 //function use to display GitHub User Info
@@ -35,5 +37,26 @@ const displayGHInfo = function(profileData) {
             <p><strong>Number of public repos:</strong> ${profileData.public_repos}</p>
         </div>
     `;
-    overview.append(div)
+    overview.append(div);
+};
+
+//async function used to collect my GitHub repos
+const getRepos = async function() {
+    const reposList = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const reposData = await reposList.json();
+    //console.log(reposData);
+
+    displayRepoInfo(reposData);
+};
+
+getRepos(); //calls function
+
+//function used to display info for each repo in user's list of repos
+const displayRepoInfo = function(repos) {
+    for( const repo of repos) {
+        const li = document.createElement("li"); //creates unoredered list item element
+        li.classList.add("repo"); //adds reop class to list item element
+        li.innerHTML = `<h3>${repo.name}</h3>`; // create a h3 HTML element for repos name
+        repoList.append(li); //displays & adds repo to end of list
+    }
 };
